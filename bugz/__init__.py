704460f553fe4d32a4dd45f461fd8b573b56912e
vrl <viorel@athabascau.ca>
2013-05-06 17:31:39 -0700

    - updated version and maitainer to reflect new tag

--- bugz/__init__.py
+++ bugz/__init__.py
@@ -19,11 +19,11 @@ Classes
 
 """
 
-__version__ = '0.9.3.1'
+__version__ = '0.9.3.2'
 __author__ = 'Alastair Tse <http://www.liquidx.net/>'
 __contributors__ = ['Santiago M. Mola <cooldwind@gmail.com',
 					'William Hubbs <w.d.hubbs@gmail.com']
-__maintainer__ = 'infrabit <infrabit@infrabit.net>'
+__maintainer__ = 'Viorel Tabara <viorel@athabascau.ca>'
 __revision__ = '$Id: $'
 __license__ = """Copyright (c) 2006, Alastair Tse, All rights reserved.
 This following source code is licensed under the GPL v2 License."""
